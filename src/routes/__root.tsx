08e124f47f914524a2d35454fc06ff6ccada4d8f
theoj246 <theoj246@gmail.com>
2026-05-06 05:49:15 +0900

    add pixel

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,20 @@ import { PostHogProvider } from "@/providers/posthog"
 
 import appCss from "../styles.css?url"
 
+const META_PIXEL_ID = "1251960360430615"
+const META_PIXEL_SCRIPT = `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${META_PIXEL_ID}');
+fbq('track', 'PageView');
+`
+
 export const Route = createRootRoute({
   head: () => ({
     meta: [
@@ -38,8 +52,18 @@ function RootDocument({ children }: { children: React.ReactNode }) {
     <html lang="ja">
       <head>
         <HeadContent />
+        <script dangerouslySetInnerHTML={{ __html: META_PIXEL_SCRIPT }} />
       </head>
       <body>
+        <noscript>
+          <img
+            alt=""
+            height="1"
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            style={{ display: "none" }}
+            width="1"
+          />
+        </noscript>
         <PostHogProvider>{children}</PostHogProvider>
         <Scripts />
       </body>
